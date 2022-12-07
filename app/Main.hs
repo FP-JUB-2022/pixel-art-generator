b65948dd61db1b1f290bf478154b7fae7d836fbd
Kirill Ivanov <kirdmiv@gmail.com>
2022-12-07 03:24:05 +0100

    Add html support

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,9 @@ module Main (main) where
 import Web.Spock
 import Web.Spock.Config
 
+import Web.Spock.Lucid (lucid)
+import Lucid
+
 import Control.Monad.Trans
 import Data.Monoid
 import Data.IORef
@@ -14,21 +17,18 @@ import PixelImage as Pixel
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
 
-main :: IO()
-main = do
-    Pixel.test "./test-images/logs.jpg"
+-- main :: IO()
+-- main = do
+--     Pixel.test "./test-images/logs.jpg"
 
--- main :: IO ()
--- main =
---     do ref <- newIORef 0
---        spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
---        runSpock 8080 (spock spockCfg app)
+main :: IO ()
+main =
+    do ref <- newIORef 0
+       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+       runSpock 8081 (spock spockCfg app)
 
--- app :: SpockM () MySession MyAppState ()
--- app =
---     do get root $
---            text "Hello World!"
---        get ("hello" <//> var) $ \name ->
---            do (DummyAppState ref) <- getState
---               visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
---               text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+app :: SpockM () MySession MyAppState ()
+app = get root $ lucid $ do
+    h1_ "Test."
+    p_ "Test <p>."
+    input_ [type_ "file"]
